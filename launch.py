f3d5fae88c9e8c695d5ac00c4566cec40a9e4e3d
AnNH <an.nguyen@heatmob.net>
2023-06-09 09:20:25 +0700

    Update argument to pass google id

--- launch.py
+++ launch.py
@@ -12,16 +12,11 @@ from helper.v2a_server import post_v2a
 from modules import cmd_args
 from modules.paths_internal import script_path, extensions_dir
 
-google_id = ''
-for i in  range(0, len(sys.argv) - 1):
-    if sys.argv[i] == '--google_id':
-        google_id = sys.argv[i+1]
-        break
-
 commandline_args = os.environ.get('COMMANDLINE_ARGS', "")
 sys.argv += shlex.split(commandline_args)
 
 args, _ = cmd_args.parser.parse_known_args()
+google_id = args.google_id
 post_v2a(google_id, "Receive request start")
 
 python = sys.executable
