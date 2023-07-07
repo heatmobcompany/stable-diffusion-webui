354f8e51c0ce3f0b033fc33c71874272e1213d0e
AnNH <an.nguyen@heatmob.net>
2023-07-07 22:44:39 +0700

    Update to restart program 5 times

---
+++ webui-colab.sh
@@ -0,0 +1,15 @@
+# #!/bin/bash
+ID=$1
+GROUP=$2
+TYPE=''
+URL=''
+delimiter="################################################################"
+
+export COMMANDLINE_ARGS="--listen --xformers --enable-insecure-extension-access --theme dark --gradio-queue --api --share --google-id $ID --group $GROUP"
+for idx in $(seq 1 5)
+do
+    echo $delimiter
+    echo Start program, time = $idx
+    echo $delimiter
+    python launch.py $COMMANDLINE_ARGS
+done
