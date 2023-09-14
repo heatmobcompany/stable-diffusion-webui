625ac02debe4dddcf4044b293b43d2bd2bab047b
AnNH <an.nguyen@heatmob.net>
2023-09-14 17:00:54 +0700

    Add cloudflared

--- webui-colab.sh
+++ webui-colab.sh
@@ -1,14 +1,10 @@
 # #!/bin/bash
 ID=$1
 GROUP=$2
-NGROK='2LMPw5i5nST1VFwmVjnnenjnQju_4Xx6KP2Y1n9bTtHw4t6fW'
-if [ -n "$3" ]; then
-    NGROK=$3
-fi
 
 delimiter="################################################################"
 
-export COMMANDLINE_ARGS="--listen --xformers --enable-insecure-extension-access --api --nowebui --port 7860 --google-id $ID --group $GROUP --ngrok $NGROK --ngrok-region ap" 
+export COMMANDLINE_ARGS="--listen --xformers --enable-insecure-extension-access --api --nowebui --port 7860 --google-id $ID --group $GROUP --cloudflared" 
 for idx in $(seq 1 5)
 do
     echo $delimiter
