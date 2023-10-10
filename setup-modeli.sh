835bedd45fadc235bad396f26326a0347d597854
AnNH <an.nguyen@heatmob.net>
2023-10-10 11:50:02 +0700

    Add controlnet to scripts

--- setup-modeli.sh
+++ setup-modeli.sh
@@ -10,7 +10,7 @@ id=$1
 url=$2
 
 # downloadextension
-# git clone https://github.com/heatmobcompany/sd-webui-controlnet /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet
+git clone https://github.com/heatmobcompany/sd-webui-controlnet /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet
 git clone https://github.com/heatmobcompany/sd-webui-segment-anything /workspace/stable-diffusion-webui/extensions/sd-webui-segment-anything
 git clone https://github.com/heatmobcompany/sd-webui-roop /workspace/stable-diffusion-webui/extensions/sd-webui-roop
 git clone https://github.com/heatmobcompany/sd-webui-adetailer /workspace/stable-diffusion-webui/extensions/sd-webui-adetailer
