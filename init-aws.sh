865b99ac022af6f9b77b8733dfac878d5b8bb69f
hoanganht91 <hoanganht91@gmail.com>
2023-11-23 09:27:35 +0000

    update

--- init-aws.sh
+++ init-aws.sh
@@ -25,15 +25,17 @@ git clone https://github.com/heatmobcompany/sd-webui-controlnet /workspace/stabl
 git clone https://github.com/heatmobcompany/sd-webui-segment-anything /workspace/stable-diffusion-webui/extensions/sd-webui-segment-anything
 git clone https://github.com/heatmobcompany/sd-webui-roop /workspace/stable-diffusion-webui/extensions/sd-webui-roop
 git clone https://github.com/heatmobcompany/sd-webui-adetailer /workspace/stable-diffusion-webui/extensions/sd-webui-adetailer
-git clone https://github.com/heatmobcompany/sd-webui-prompt-all-in-one /workspace/stable-diffusion-webui/extensions/sd-webui-prompt-all-in-one
+# git clone https://github.com/heatmobcompany/sd-webui-prompt-all-in-one /workspace/stable-diffusion-webui/extensions/sd-webui-prompt-all-in-one
 git clone https://github.com/heatmobcompany/Civitai-Helper /workspace/stable-diffusion-webui/extensions/Civitai-Helper
 git clone https://github.com/heatmobcompany/openpose-editor /workspace/stable-diffusion-webui/extensions/openpose-editor
 git clone https://github.com/heatmobcompany/stable-diffusion-webui-rembg /workspace/stable-diffusion-webui/extensions/stable-diffusion-webui-rembg
 
-$SCRIPT_DIR/setup-checkpoint.sh all
-$SCRIPT_DIR/setup-common.sh
+# $SCRIPT_DIR/setup-checkpoint.sh all
+# $SCRIPT_DIR/setup-common.sh
 
 # create and add config.yaml
 if [ ! -f /workspace/config.yaml ]; then
     cp /workspace/stable-diffusion-webui/config.yaml /workspace/config.yaml
 fi
+
+$SCRIPT_DIR/job_healthcheck.sh install
