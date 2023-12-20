289a8faf3848e801cb6e796a1209b3de9d8efc6a
hoanganht91 <hoanganht91@gmail.com>
2023-12-20 09:05:34 +0700

    Fix error

--- init-aws.sh
+++ init-aws.sh
@@ -28,8 +28,9 @@ git clone https://github.com/heatmobcompany/sd-webui-roop /workspace/stable-diff
 git clone https://github.com/heatmobcompany/sd-webui-adetailer /workspace/stable-diffusion-webui/extensions/sd-webui-adetailer
 # git clone https://github.com/heatmobcompany/sd-webui-prompt-all-in-one /workspace/stable-diffusion-webui/extensions/sd-webui-prompt-all-in-one
 git clone https://github.com/heatmobcompany/Civitai-Helper /workspace/stable-diffusion-webui/extensions/Civitai-Helper
-git clone https://github.com/heatmobcompany/openpose-editor /workspace/stable-diffusion-webui/extensions/openpose-editor
+# git clone https://github.com/heatmobcompany/openpose-editor /workspace/stable-diffusion-webui/extensions/openpose-editor
 git clone https://github.com/heatmobcompany/stable-diffusion-webui-rembg /workspace/stable-diffusion-webui/extensions/stable-diffusion-webui-rembg
+git clone https://github.com/heatmobcompany/sd-webui-openpose-editor /workspace/stable-diffusion-webui/extensions/sd-webui-openpose-editor
 
 $SCRIPT_DIR/setup-checkpoint.sh all
 $SCRIPT_DIR/setup-common.sh
--- javascript/ui.js
+++ javascript/ui.js
@@ -289,7 +289,7 @@ function replaceAll(input, from, to) {
 function submit() {
     // console.log('Submit txt2img')
     window.parent?.postMessage({ message: "logEvent", name: "generate_button_click", data: { button_id: 'txt2img_generate', button_text: 'Generate' } }, "*");
-    // checkCredit();
+    checkCredit();
     showSubmitButtons('txt2img', false);
 
     var id = randomId();
@@ -324,7 +324,7 @@ function submit_img2img() {
     // console.log('Submit img2img')
     window.parent?.postMessage({ message: "logEvent", name: "generate_button_click", data: { button_id: 'img2img_generate', button_text: 'Generate' } }, "*");
 
-    // checkCredit();
+    checkCredit();
     showSubmitButtons('img2img', false);
 
     var id = randomId();
@@ -360,7 +360,7 @@ function submit_extras() {
     // console.log('Submit extras')
     window.parent?.postMessage({ message: "logEvent", name: "generate_button_click", data: { button_id: 'extras_generate', button_text: 'Generate' } }, "*");
 
-    // checkCredit();
+    checkCredit();
 
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get('token');
