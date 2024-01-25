fb7a83ecd70ca01595324bad498b7fec14dbbb80
hoanganht91 <hoanganht91@gmail.com>
2024-01-25 09:29:41 +0700

    Change api docs

--- webui.py
+++ webui.py
@@ -89,8 +89,8 @@ def webui():
             prevent_thread_lock=True,
             allowed_paths=cmd_opts.gradio_allowed_path,
             app_kwargs={
-                "docs_url": "/docs",
-                "redoc_url": "/redoc",
+                "docs_url": "/xdocs",
+                "redoc_url": "/xredoc",
             },
             root_path=f"/{cmd_opts.subpath}" if cmd_opts.subpath else "",
         )
