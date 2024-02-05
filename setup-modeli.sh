7e1828c8446e4a77afaf6f13b94af094fa4171a7
hoanganht91 <hoanganht91@gmail.com>
2024-02-05 09:45:30 +0000

    Update script setup

--- setup-modeli.sh
+++ setup-modeli.sh
@@ -1,41 +1,5 @@
 #!/usr/bin/env bash
 
-id=""
-group=Majicmix
-type="fashion_app_pod"
-url=""
-
-# Parse command line options
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    -i | --id)
-      id="$2"
-      shift 2
-      ;;
-    -g | --group)
-      group="$2"
-      shift 2
-      ;;
-    -t | --type)
-      type="$2"
-      shift 2
-      ;;
-    -u | --url)
-      url="$2"
-      shift 2
-      ;;
-    *)
-      echo "Invalid option: $1"
-      exit 1
-      ;;
-  esac
-done
-
-echo id:$id
-echo group:$group
-echo type:$type
-echo url:$url
-
 # setup extension
 git clone https://github.com/heatmobcompany/sd-webui-controlnet /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet
 git clone https://github.com/heatmobcompany/sd-webui-segment-anything /workspace/stable-diffusion-webui/extensions/sd-webui-segment-anything
@@ -54,22 +18,9 @@ aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ann
 aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/beautifulRealistic_v40.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o beautifulRealistic_v40.safetensors &\
 aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/photon_v1.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o photon_v1.safetensors
 aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/cyberrealistic_v40-inpainting.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o cyberrealistic_v40-inpainting.safetensors
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/vnf19.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o vnf19.safetensors
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-"${SCRIPT_DIR}/setup-checkpoint.sh" $group
-"${SCRIPT_DIR}/setup-common.sh"
+# setup lora
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/vnfs_lr.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o vnfs_lr.safetensors
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/suanbei_v7-000002.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o suanbei_v7-000002.safetensors
 
-# Update webui-user.sh
-file_path="./webui-user.sh"
-if [ -n "$group" ]; then
-    sed -i "s|^GROUP=.*|GROUP=$group|" "$file_path"
-fi
-if [ -n "$type" ]; then
-    sed -i "s|^TYPE=.*|TYPE=$type|" "$file_path"
-fi
-if [ -n "$id" ]; then
-    sed -i "s|^ID=.*|ID=$id|" "$file_path"
-fi
-if [ -n "$url" ]; then
-    sed -i "s|^URL=.*|URL=$url|" "$file_path"
-fi
