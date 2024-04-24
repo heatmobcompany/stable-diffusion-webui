bda9c93e47fc7ce5807ec109a45014e3571f388e
hoanganht91 <hoanganht91@gmail.com>
2024-04-24 03:24:04 +0000

    Update scripts

--- update-webui.sh
+++ update-webui.sh
@@ -2,6 +2,7 @@
 
 cd /workspace/sd-worker || exit
 docker compose pull
+curl http://172.17.0.1:3003/stop-worker
 sudo service sd-service restart
 sleep 1
 docker compose down
--- update-worker.sh
+++ update-worker.sh
@@ -2,5 +2,6 @@
 
 cd /workspace/sd-worker || exit
 docker compose pull
+curl http://172.17.0.1:3003/stop-worker
 docker compose down
 docker compose up -d
