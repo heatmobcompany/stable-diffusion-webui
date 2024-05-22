33f52167854c56ca7243f8ce2551fe76602e797f
hoanganht91 <hoanganht91@gmail.com>
2024-05-22 09:59:42 +0000

    Update model checkpoint and lora

--- setup-modeli.sh
+++ setup-modeli.sh
@@ -16,11 +16,16 @@ aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ann
 aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/chilloutmix_NiPrunedFp32.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o chilloutmix_NiPrunedFp32.safetensors &\
 aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/kencanmix_v20Beta.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o kencanmix_v20Beta.safetensors &\
 aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/beautifulRealistic_v40.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o beautifulRealistic_v40.safetensors &\
-aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/photon_v1.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o photon_v1.safetensors
-aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/cyberrealistic_v40-inpainting.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o cyberrealistic_v40-inpainting.safetensors
-aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/vnf19.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o vnf19.safetensors
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/photon_v1.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o photon_v1.safetensors &\
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/cyberrealistic_v40-inpainting.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o cyberrealistic_v40-inpainting.safetensors &\
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/vnf19.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o vnf19.safetensors &\
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/realisticVisionV60B1_v51VAE_2G.safetensors -d /workspace/stable-diffusion-webui/models/Stable-diffusion -o realisticVisionV60B1_v51VAE_2G.safetensors &\
+echo "Download checkpoint done"
 
 # setup lora
-aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/vnfs_lr.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o vnfs_lr.safetensors
-aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/suanbei_v7-000002.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o suanbei_v7-000002.safetensors
-
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/vnfs_lr.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o vnfs_lr.safetensors &\
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/suanbei_v7-000002.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o suanbei_v7-000002.safetensors &\
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/vnman4.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o vnman4.safetensors &\
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/bgw.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o bgw.safetensors &\
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/annh/general/resolve/main/chouchou-000005.safetensors -d /workspace/stable-diffusion-webui/models/Lora -o chouchou-000005.safetensors &\
+echo "Download lora done"
